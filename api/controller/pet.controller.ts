afbf5039ee5abe22c38884a5105f878ea2277560
Magnifikate <46503458+WannaBeDream@users.noreply.github.com>
2022-07-09 15:27:58 +0300

    third time updates

--- api/controller/pet.controller.ts
+++ api/controller/pet.controller.ts
@@ -1,20 +1,36 @@
 import axios from "axios";
-import {URLSearchParams} from 'url';
+import { URLSearchParams } from "url";
+import {definitions, operations} from "../../.temp/types"
 
 export class PetController {
-    instance:any = axios.create({
-        baseURL: 'https://petstore.swagger.io/v2/pet/',
-        timeout: 1000,
-        headers: {'X-Custom-Header': 'foobar'}
-      });
-    
-    async getById(id:string | string): Promise<any> {
-        const response = await this.instance.get(id);
-        return response.data;
-    }
-    async findByStatus(status:string | string[]): Promise<any> {
-        const response = await this.instance.get(`findByStatus`, {params:new URLSearchParams({ status })}); // will be transformed into encoded path: findByStatus?status=available
-        return response.data;
-    }
+  instance: any = axios.create({
+    baseURL: "https://petstore.swagger.io/v2/pet/",
+    timeout: 1000,
+    headers: { "X-Custom-Header": "foobar" },
+  });
 
+  async getById(id: number | string): Promise<operations["getPetById"]["responses"]["200"]["schema"]> { // correct example
+    const response = await this.instance.get(`${id}`);
+    return response.data;
+  }
+  async findByStatus(status: string | string[]): Promise<operations["findPetsByStatus"]["responses"]["200"]["schema"]> {
+    const response = await this.instance.get(`findByStatus`, {
+      params: new URLSearchParams({ status }),
+    }); 
+    return response.data;
+  }
+
+  async addNew(pet: Omit<definitions["Pet"], "id">): Promise<definitions["Pet"]>{
+    const response = await this.instance.post(``, pet); 
+    return response.data;
+  }
+  async update(pet: definitions["Pet"]): Promise<definitions["Pet"]>{
+    const response = await this.instance.put(``, pet); 
+    return response.data;
+  }
+
+  async deleteById(id: string | number): Promise<definitions["ApiResponse"]> {
+    const response = await this.instance.delete(`${id}`);
+    return response.data;
+  }
 }
--- tests/pet.test.ts
+++ tests/pet.test.ts
@@ -1,27 +1,92 @@
-import axios from "axios";
 import { strict as assert } from "assert";
-import { URLSearchParams } from "url";
 import { PetController } from "./../api/controller/pet.controller";
+import {definitions, operations} from "../.temp/types"
 
 const pet = new PetController();
 
 describe("User can", () => {
-
   it("receive pet by his id", async () => {
-    const result = await pet.getById("3");
+    const result = await pet.getById("10");
     assert(
-      result.id === 3,
-      `Expected response with id equlas 3, but got ${result.id}`
+      result.id === 10,
+      `Expected response with id equlas 10, but got ${result.id}`
     );
   });
 
   it("find pets by pending status", async () => {
     const result = await pet.findByStatus("pending");
     assert(
-      result.every((pet:any)=> {
-        return pet.status === "pending"
+      result.every((pet) => {
+        return pet.status === "pending";
       }),
       `Every pet in the response must contain pending status`
     );
+  });
+
+  it("can be added, updated and deleted", async () => {
+    const petToCreate: Omit<definitions["Pet"], "id"> = {  // Omit generic to create new type without "id"
+      category: {
+        id: 98761230,
+        name: "custom_category_dsadasd",
+      },
+      name: "Ilon mask",
+      photoUrls: ["https//example.com"],
+      tags: [
+        {
+          id: 98761230,
+          name: "custom_qweasdsdvfdv",
+        },
+      ],
+      status: "available",
+    };
+
+    const addedPet = await pet.addNew(petToCreate);
+    assert.deepEqual(
+      addedPet,
+      {
+        ...petToCreate,
+        id: addedPet.id,
+      },
+      "Expected created pet to match data used upon creation"
+    );
+
+    const foundaddedPet = await pet.getById(addedPet.id as string | number); // issue 404 here ( API bug )
+
+    assert.deepEqual(
+      foundaddedPet,
+      {
+        ...petToCreate,
+        id: addedPet.id,
+      },
+      "Expected created pet to match found pet"
+    );
+
+    const petToUpdate: definitions["Pet"] = {
+      id: addedPet.id,
+      category: {
+        id: 98761230,
+        name: "custom_category_dsadasd",
+      },
+      name: "SpaceX",
+      photoUrls: ["https//example.com"],
+      tags: [
+        {
+          id: 98761230,
+          name: "custom_qweasdsdvfdv",
+        },
+      ],
+      status: "available",
+    };
+    const updatedPet = await pet.update(petToUpdate); // issue 500 here ( API bug )
+
+    assert.deepEqual(
+      updatedPet,
+      petToUpdate,
+      "Expected updated pet to equal data used upon updating"
+    );
+
+    await pet.deleteById(addedPet.id as string | number) // issue 404 ( API bug)
+
+    // TODO assert 404 for delete action 
   });
 });
